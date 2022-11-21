8cfd13d9c7001eba0355e74be879f9e48fffb144
Lazarew03 <lazurich1@gmail.com>
2022-11-21 16:47:48 +0300

    Initial commit

---
+++ src/main/kotlin/Jobs.kt
@@ -0,0 +1,4 @@
+data class Jobs(var name: String, var jobTitle : String)
+{
+    var id: Int? = null
+}
---
+++ src/main/kotlin/JobsRepository.kt
@@ -0,0 +1,39 @@
+class JobsRepository {
+    val LJobss = mutableListOf<Jobs>()
+
+    fun findById(id: Int) : Int?{
+        var findIndex: Int? = null
+        for( i in 0..LJobss.size){
+            if(LJobss[i].id == id){
+                findIndex = i
+                println("Работник ${LJobss[i].name} найден")
+            } else {
+                findIndex = null
+                println("Работник не найден")
+            }
+            break
+        }
+        return findIndex
+    }
+
+    fun insert(jobs: Jobs) {
+        jobs.id = LJobss.size
+        print("id: " + jobs.id + " ")
+        LJobss.add(jobs)
+        println("${jobs.name} добвален")
+    }
+
+    fun update(id: Int, jobs: Jobs) {
+        var searchJobsPhone: Int? = findById(id)
+        if (searchJobsPhone != null) {
+            jobs.id = id
+            LJobss[searchJobsPhone] = jobs
+        }
+    }
+    fun delete(id: Int) {
+        var jobs: Int? = findById(id)
+        if (jobs != null) {
+            LJobss.removeAt(jobs)
+        }
+    }
+}
